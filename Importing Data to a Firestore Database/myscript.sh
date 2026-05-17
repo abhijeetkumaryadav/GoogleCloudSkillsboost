ea0d7dca2a7b73bb4bf07b03f66770f9f2103037
abhijeetkumaryadav <abhijeethkumaryadav@gmail.com>
2026-05-17 14:43:52 +0530

    Add comments to script for clarity

--- Importing Data to a Firestore Database/myscript.sh
+++ Importing Data to a Firestore Database/myscript.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Get Project ID
+# 1. Get Project ID
 export PROJECT_ID=$(gcloud config get-value project)
 
-# Enable Firestore
+# 2. Enable Firestore API
 gcloud services enable datastore.googleapis.com --project=$PROJECT_ID
 
-# Direct Download (This fixes the GitHub Username error)
+# 3. Direct Download (This fixes the GitHub Username error)
 wget https://raw.githubusercontent.com/robmorgan/firestore-import-export/master/import.js
 wget https://raw.githubusercontent.com/robmorgan/firestore-import-export/master/package.json
 
-# Install and Run
+# 4. Install and Run
 npm install
 gsutil cp gs://cloud-training/pureskill/firestore/customers.json .
 node import.js customers.json
